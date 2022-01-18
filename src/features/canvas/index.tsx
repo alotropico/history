fad1b2e96dc2d602fe36873d3e598e918e567c60
Leandro Vieira Steffen <lsteffen@interactions.com>
2022-01-18 17:50:58 -0300

    SavePoint

--- src/features/canvas/index.tsx
+++ src/features/canvas/index.tsx
@@ -1,8 +1,14 @@
 import Item from './components/Item'
 import useParsedItems from './hooks/useParsedItems'
 import { ICanvas } from './types'
+import style from './style/Canvas.module.scss'
 
 export default function Canvas({ items }: ICanvas) {
-  const parsedItems = useParsedItems(items, -580, -281)
-  return <table>{parsedItems.map((item) => item?.name && <Item key={item.id} {...item} />)}</table>
+  const bounds = [-580, -281]
+  const parsedItems = useParsedItems(items, bounds[0], bounds[1])
+  return (
+    <table className={style.canvas}>
+      <tbody>{parsedItems.map((item) => item?.name && <Item key={item.id} {...item} />)}</tbody>
+    </table>
+  )
 }
