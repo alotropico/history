7f8dea3a4d1c8a95f2a924347a0bb2ca1f539c5e
Leandro Vieira <alotropico@gmail.com>
2022-01-21 03:41:51 -0300

    SavePoint

--- src/features/canvas/index.tsx
+++ src/features/canvas/index.tsx
@@ -4,7 +4,8 @@ import { ICanvas } from './types'
 import style from './style/Canvas.module.scss'
 
 export default function Canvas({ items }: ICanvas) {
-  const [parsedItems, layers] = useParsedItems(items, -580, -281)
+  const bounds = [-580, -281]
+  const [parsedItems, layers] = useParsedItems(items, bounds[0], bounds[1])
   return <div className={style.canvas}>{parsedItems.map((item) => item?.name && <Item key={item.id} {...item} />)}</div>
   // return <table>{parsedItems.map((item) => item?.name && <Item key={item.id} {...item} />)}</table>
 }
